f09bd793f5a49053552535a4692767ba62b1fd76
yangkuo <734271234@qq.com>
2018-03-26 16:09:13 +0800

    axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,22 @@
 import axios from 'axios'
-var $http = axios.create({
+const $http = axios.create({
   baseURL: 'https://cnodejs.org/api/v1',
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
+})
+// 请求拦截
+$http.interceptors.request.use((config) => {
+  console.log(config)
+  return config
+}, (error) => {
+  return Promise.reject(error)
+})
+
+// 响应拦截
+$http.interceptors.response.use((response) => {
+  console.log(response)
+  return response.data
+}, (error) => {
+  return Promise.reject(error)
 })
 export default $http
